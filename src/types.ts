7c35c60e2c0c6865e3deacc9afbb643b7494ba67
Ryan Luong <lumintriet@gmail.com>
2026-09-25 18:55:38 +0700

    feat(types): define NestedErrorObject type.

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import type { BaseErrorType } from "@little-nebulae/error";
+import type { OmitKnownKeys } from "@little-nebulae/type-utils";
 import type { JSONType } from "zod";
 
 export type ErrorObjectMeta = Record<string, JSONType> | null;
@@ -7,5 +8,13 @@ export interface FlatErrorObject<
   TCode extends string,
   TMeta extends ErrorObjectMeta = null,
 > extends Pick<BaseErrorType<TCode>, "name" | "message" | "code"> {
+  meta: TMeta;
+}
+
+export interface NestedErrorObject<
+  TCode extends string,
+  TMeta extends ErrorObjectMeta = null,
+> extends OmitKnownKeys<BaseErrorType<TCode>, "cause" | "meta"> {
+  cause: JSONType;
   meta: TMeta;
 }
